266373792a6b51697d412d96516289f572fecaf7
webdeveloper <83573049+webdeveloperintern@users.noreply.github.com>
2023-08-19 08:43:50 +0530

    Create difference.java

---
+++ difference.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Main {
+  public static void main(String[] args) {
+    int []arr={7,9,5,6,3,2};
+    Arrays.sort(arr);
+    int temp;
+    for(int i=0;i<arr.length;i++)
+    {
+        for (int j = i+1; j < arr.length; j++) {     
+      if(arr[j]>arr[i])
+      {
+        temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }}
+    }
+    int diff = arr[0]-arr[1];
+      System.out.println("Difference = "+diff);
+  }
+}
