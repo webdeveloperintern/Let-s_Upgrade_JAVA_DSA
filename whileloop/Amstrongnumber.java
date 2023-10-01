84ad949528a344a4b132a6a48ffc22d017d924bb
webdeveloper <83573049+webdeveloperintern@users.noreply.github.com>
2023-10-01 09:06:15 +0000

    Java_DSA

---
+++ whileloop/Amstrongnumber.java
@@ -0,0 +1,34 @@
+package whileloop;
+
+import java.util.Scanner;
+
+public class Amstrongnumber {
+    public static void main(String[] args) {
+        System.out.println("AmstrongNumber : ");
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        int digit =number,result = 0,ndigits=0,Nnumber=number;
+        while(Nnumber !=0)
+        {
+            int reminder = Nnumber % 10;
+            ndigits++;
+            Nnumber /=10;
+        }
+        while(number != 0){
+            int reminder = number % 10;
+            int temp =(int) Math.pow(reminder,ndigits);
+            result +=temp;
+            number/=10;
+        }
+        if(digit == result)
+        {
+            System.out.println(digit + " is a Amtrong Number");
+        }
+        else
+        {
+            System.out.println(digit + " is not a Amstrong Number");
+        }
+        sc.close();
+
+    }
+}
