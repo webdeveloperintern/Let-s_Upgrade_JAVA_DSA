372704daf9d19d0405013c412c67cfe6ac6aad35
webdeveloper <83573049+webdeveloperintern@users.noreply.github.com>
2023-10-01 08:19:41 +0000

    gtoit

--- whileloop/digitsum.java
+++ whileloop/digitsum.java
@@ -1,7 +1,19 @@
 package whileloop;
 
+import java.util.Scanner;
+
 public class digitsum{
     public static void main(String[] args) {
-        
+        System.out.print("Enter the number : ");
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        int reverse = 0,sum=0;
+        while(number != 0){
+            reverse = number % 10;
+            sum += reverse;
+            number = number / 10;
+        }
+        System.out.println("Sum of the DIgits is : "+sum);
+        sc.close();
     }
 }
