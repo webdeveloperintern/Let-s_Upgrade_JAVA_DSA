334d1469ee43d1f643c25d124eacc4ef790b709b
webdeveloper <83573049+webdeveloperintern@users.noreply.github.com>
2023-10-22 10:38:21 +0530

    Update UniqueCharacter.java

--- practice/Strings/Google/UniqueCharacter.java
+++ practice/Strings/Google/UniqueCharacter.java
@@ -15,3 +15,21 @@ class NonRepeate{
                 // System.out.println("the First Non Repeating Character in the given word "+word+" is : "+NoRepeate);
     }
 }
+
+
+//Non NonRepeateanner all chara
+import java.util.Scanner;
+
+class NonRepeate{
+    public static void main (String[] args) {
+        System.out.print("Enter the String");
+        Scanner sc = new Scanner(System.in);
+        String word = sc.nextLine();
+        for(char repeat:word.toCharArray()){
+            if(word.indexOf(repeat) == word.lastIndexOf(repeat)){
+                System.out.println("the First Non Repeating Character in the given word "+word+" is : "+repeat);
+            }
+        }
+                // System.out.println("the First Non Repeating Character in the given word "+word+" is : "+NoRepeate);
+    }
+}
